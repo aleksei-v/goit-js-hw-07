e41b8ae2ce29204dd27dab2ed5d688a60e0b9fa3
aleksei-v <aleksey.vasilenko7@gmail.com>
2022-07-24 15:15:43 +0300

    add gallery with js library (task 2)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const makeImageGallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createLi(images) {
+    return images.reduce((acc, { preview, description, original }) => acc + `
+  <a class="gallery__item" 
+  href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}"
+    </a> `
+  , "")
+}
+const result = createLi(galleryItems);
+
+makeImageGallery.insertAdjacentHTML('beforeend', result);
+
+
+function handleClick(evt) {
+  evt.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        captionsData: 'alt',
+        captionPosition: "top",
+    });
+}   
+makeImageGallery.addEventListener('click', handleClick);
