883aca3ddee744076cfc05bf0ae26190bdf6b8d9
aleksei-v <aleksey.vasilenko7@gmail.com>
2022-07-24 13:04:25 +0300

    add lightbox in 1st html file

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,29 +1,57 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const makeImageGallery = document.querySelector(".gallery");
-const rowImageGallery = ({ preview, description }) => `<div class="gallery__item">
-  <a class="gallery__link" href="${preview}">
+
+// const rowImageGallery = ({ preview, description, original }) => `<div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div> `
+
+// const imageGallery = galleryItems
+//   .map(rowImageGallery)
+//   .join(" ");
+    
+function createLi(images) {
+  return images.reduce((acc, { preview, description, original }) => acc + `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="large-image.jpg"
+      data-source="${original}"
       alt="${description}"
     />
   </a>
 </div> `
-
-const imageGallery = galleryItems
-    .map(rowImageGallery)
-    .join(" ")
-
-makeImageGallery.insertAdjacentHTML('beforeend', imageGallery);
-
-
-const handleClick = (evt) => {
-    evt.preventDefault()
-    console.log(evt.target.src);
-
+  , "")
 }
+const result = createLi(galleryItems);
 
+makeImageGallery.insertAdjacentHTML('beforeend', result);
+
+
+function handleClick(evt) {
+  evt.preventDefault();
+  if (evt.target.classList.contains('gallery__image')) {
+    const onPressEsc = (evt) => {
+      if (evt.code === 'Escape') {
+        modalImage.close()
+      }
+    }
+    const modalImage = basicLightbox.create(
+      `<img src="${evt.target.dataset.source}" width="800" height="600"/>`,
+      {
+        onShow: () => window.addEventListener('keydown', onPressEsc),
+        onClose: () => window.removeEventListener('keydown', onPressEsc),
+      }
+    );
+    modalImage.show();
+  }        
+}   
 makeImageGallery.addEventListener('click', handleClick);
+
